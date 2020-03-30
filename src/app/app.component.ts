d6a4c335bafaa43f4da7924d95e29cee59759129
priyanka-jadhav30 <jadhavpriyanka095@gmail.com>
2020-03-30 14:32:11 +0530

    Applied bulma css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div class="navbar is-dark">
+      <div class="navbar-brand">
+      <div>
+      <img src="assets/img/shop_logo.png"/>
+      </div>
+            <a class="navbar-item">
+            Welcome to online shopping
+            </a>
+      </div>
+  </div>
+  
+  
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myAngularApp';
+  title = 'app';
 }
